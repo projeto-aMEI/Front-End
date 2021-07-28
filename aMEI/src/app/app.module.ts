7d04ea3414e0c9f8cecc6aa36d1348d1f840c06c
talu <talita.santos96@hotmail.com>
2021-07-28 14:33:04 -0300

    28.07.2021 | Criando rotas

--- aMEI/src/app/app.module.ts
+++ aMEI/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { RodapeComponent } from './rodape/rodape.component';
 import { MenuComponent } from './menu/menu.component';
 import { EntrarComponent } from './entrar/entrar.component';
@@ -16,7 +17,8 @@ import { RegistrarComponent } from './registrar/registrar.component';
     RegistrarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
