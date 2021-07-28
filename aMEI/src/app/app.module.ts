b91177a35e549e081bccbeb127d53c9f16098dea
joaovitormo <vitormendes388@gmail.com>
2021-07-28 14:17:33 -0300

    28/07/2021 - Adicionando o Sobre nós e Contato

--- aMEI/src/app/app.module.ts
+++ aMEI/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { MenuComponent } from './menu/menu.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RodapeComponent,
-    MenuComponent
+    MenuComponent,
+    SobreNosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule
